6f573b6377adb7777bd2a8dcb3b0a85c2dfeacac
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-11 16:57:16 -0500

    lab completed

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,30 +3,59 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    private int shift;
 
     ROT13(Character cs, Character cf) {
+        this.shift = cf - cs;
     }
 
     ROT13() {
+        this.shift = 13;
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder crypted = new StringBuilder();
 
-        return "";
+        for(int i = 0; i < text.length(); i++) {
+            char t = text.charAt(i);
+
+            if(!Character.isAlphabetic(t)){
+                crypted.append(t);
+                continue;
+            }
+            if ((Character.isUpperCase(t) && t > 'M') && t < 'Z') {
+                t -= shift;
+            } else if ((Character.isLowerCase(t) && t > 'm') && t < 'z'){
+                t -= shift;
+            } else {
+                t += shift;
+            }
+            crypted.append(t);
+        }
+
+        return crypted.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        if (s.charAt(0) == c) {
+            return s;
+        }
 
-        return "";
+        int position = s.indexOf(c);
+
+        String start = s.substring(position);
+        String end = s.substring(0, position);
+
+        return start+end;
     }
 
 }
